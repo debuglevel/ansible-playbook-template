a7c143ffc8bdb21b8dbfb9c5176fc282a827c936
Marc Kohaupt <dbeuglevel@gmail.com>
2022-02-04 21:28:32 +0000

    Remove deprecated Eclipse Theia

--- run-theia-ide.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-USER=1002
-PORT=1234
-echo "Starting theia as docker container..."
-sudo docker run -d --user $USER --name theia --init -p $PORT:3000 -v "${PWD}:/home/project:cached" theiaide/theia:latest
