2d8d315e8814c18b9333cf6659213934b14005b1
Marc Kohaupt <debuglevel@gmail.com>
2022-06-02 23:09:50 +0200

    Reformat Python files

--- supplemental/print-inventory-hosts.py
+++ supplemental/print-inventory-hosts.py
@@ -7,21 +7,20 @@ from ansible.parsing.dataloader import DataLoader
 from ansible.inventory.manager import InventoryManager
 import pprint
 
-if __name__ == '__main__':
-    if (len(sys.argv) < 1+1):
+if __name__ == "__main__":
+    if len(sys.argv) < 1 + 1:
         print("Usage: print-inventory-hosts.py <inventory file>")
         exit(255)
 
     inventory_file_name = sys.argv[1]
-    print(f"Using inventory file '{inventory_file_name}'...", file = sys.stderr)
+    print(f"Using inventory file '{inventory_file_name}'...", file=sys.stderr)
 
     data_loader = DataLoader()
-    inventory = InventoryManager(loader = data_loader,
-                                 sources = [inventory_file_name])
+    inventory = InventoryManager(loader=data_loader, sources=[inventory_file_name])
 
-    #for host in inventory.get_groups_dict()['all']:
+    # for host in inventory.get_groups_dict()['all']:
     #    print(host)
 
     for host in inventory.get_hosts():
         print(host)
-        #print(host.vars['ansible_host'])
+        # print(host.vars['ansible_host'])
