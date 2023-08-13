3009d3f4201ba67c1d6f6e77072ca9802acf6bdb
Marc Kohaupt <debuglevel@gmail.com>
2023-08-13 16:58:23 +0200

    feat: various improvements from other repo

--- supplemental/authenticate-hosts.sh
+++ supplemental/authenticate-hosts.sh
@@ -8,6 +8,5 @@
 SCRIPTPATH=$(dirname "$(readlink -f "$0")")
 while read host
 do
-    echo "Processing $host ..."
-    "$SCRIPTPATH"/authenticate-host.sh $host
+    $SCRIPTPATH/authenticate-host.sh $host
 done < "${1:-/dev/stdin}"
